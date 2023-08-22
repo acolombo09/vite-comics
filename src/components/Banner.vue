7a7941aeb54e4e60420736dfb18be2888108d8d6
acolombo09 <acolombo0911@gmail.com>
2023-08-22 23:16:45 +0200

    banner_details

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -16,27 +16,27 @@ export default {
             </div>
           </div>
           <div class="col d-flex align-items-center">
-            <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+            <img src="../assets/img/buy-comics-merchandise.png" alt="merchandise-comics">
             <div>
-              <h3 class="mb-0 ms-3 fs-6 text-white">DIGITAL COMICS</h3>
+              <h3 class="mb-0 ms-3 fs-6 text-white">DC MERCHANDISE</h3>
             </div>
           </div>
           <div class="col d-flex align-items-center">
-            <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+            <img src="../assets/img/buy-comics-subscriptions.png" alt="sub-comics">
             <div>
-              <h3 class="mb-0 ms-3 fs-6 text-white">DIGITAL COMICS</h3>
+              <h3 class="mb-0 ms-3 fs-6 text-white">SUBSCRIPTION</h3>
             </div>
           </div>
           <div class="col d-flex align-items-center mx-auto">
-            <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+            <img src="../assets/img/buy-comics-shop-locator.png" alt="shop-locator-comics">
             <div>
-              <h3 class="mb-0 ms-3 fs-6 text-white">DIGITAL COMICS</h3>
+              <h3 class="mb-0 ms-3 fs-6 text-white">COMIC SHOP LOCATOR</h3>
             </div>
           </div>
           <div class="col d-flex align-items-center">
-            <img src="../assets/img/buy-comics-digital-comics.png" alt="digital-comics">
+            <img src="../assets/img/buy-dc-power-visa.svg" alt="powervisa-comics">
             <div>
-              <h3 class="mb-0 ms-3 fs-6 text-white">DIGITAL COMICS</h3>
+              <h3 class="mb-0 ms-3 fs-6 text-white">DC POWER VISA</h3>
             </div>
           </div>
         </div>
