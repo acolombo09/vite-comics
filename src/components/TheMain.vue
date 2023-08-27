2dc39aac3a5cd03ad5cce93fd579a6c594e0cba4
acolombo09 <acolombo0911@gmail.com>
2023-08-27 18:56:52 +0200

    dynamic_cards_added

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,17 +1,37 @@
 <script>
-export default {
+import comicsList from "../db/comics";
 
-}
+export default {
+  data() {
+    return {
+      comicsList
+    };
+  },
+};
 </script>
 
 <template>
   <main>
-    <div class="row">
-      <div class="col-md-9 d-flex justify-content-center align-items-center justify-content-between mx-auto px-0">
-        
+    
+  </main>
+  <div class="container-fluid cards-list">
+      <div class="row">
+        <div class="col-md-9 mx-auto px-0">
+          <div class="row row-cols-6 g-3">
+            <div class="col" v-for="(comic, i) in comicsList" v-bind:key="`comic_${i}`">
+              <div class="card border-0 rounded-0">
+                <img v-bind:src="comic.thumb" class="card-img-top" alt="">
+                <div class="card-body">
+                  <h5 class="card-title">
+                    {{ comic.series }}
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </main>
 </template>
 
 <style lang="scss" scoped>
@@ -19,12 +39,24 @@ export default {
 @use "../styles/partials/variables" as *;
 
 main{
-  min-height: 500px;
+  min-height: 390px;
   padding-top: 3rem;
   padding-bottom: 3rem;
   background-image: url(../assets/img/jumbotron.jpg);
-  background-size: auto;
+  background-size: cover;
   background-repeat: no-repeat;
+}
+
+.cards-list{
+  min-height: 500px;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+  background-color: $color-secondary;
+}
+
+.card {
+  width: 120px;
+  height: 120px;
 }
 
 </style>
